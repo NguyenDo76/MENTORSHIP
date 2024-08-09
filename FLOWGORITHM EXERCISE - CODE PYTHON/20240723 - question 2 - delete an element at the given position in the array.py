f5dbdfce8ca362fda3eef69704d9676c86b902ce
ThanhNguyen <156637599+NguyenDo76@users.noreply.github.com>
2024-08-10 00:20:58 +0700

    Update 20240723 - question 2 - delete an element at the given position in the array.py

--- FLOWGORITHM EXERCISE - CODE PYTHON/20240723 - question 2 - delete an element at the given position in the array.py
+++ FLOWGORITHM EXERCISE - CODE PYTHON/20240723 - question 2 - delete an element at the given position in the array.py
@@ -3,11 +3,11 @@ def DeleteElement (n,position,a):
   if position < 0 or position >= n:
     print ('Position out of range')
   else:
-    Newa = [0] * (n-1) #if Newa = [] * (n-1)(read Gemini that know why difference)
+    Newa = [0] * (n-1)
     for i in range (0,position-1 + 1,1):
-      Newa[i] = a[i] # if Newa = [] * (n-1) -> Newa.append(a[i])(read Gemini that know why difference)
+      Newa[i] = a[i]
     for i in range (position,n-2 + 1,1):
-      Newa[i] = a[i+1]# if Newa = [] * (n-1) -> Newa.append(a[i+1]) (read Gemini that know why difference)
+      Newa[i] = a[i+1]
     print ('Array after deletion: ')
     PrintArray (n-1,Newa)
 
@@ -19,7 +19,7 @@ def PrintArray (n,a):
 print ('Given an integer n, create an array with n random integers from 0 to 9. Delete an element at the given position in the array.')
 print ('Enter number n = ')
 n = int (input ())
-a = []  # Initialize a as an empty list
+a = []
 for i in range (0,n-1 + 1,1):
   a.append(rd.randint (0,9))
 print ('Original array: ')
